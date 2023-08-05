520029bb248f67d303934d461caa84d3244cae01
DucretJe <github@ducret.mozmail.com>
2023-08-05 16:59:07 +0200

    lint

--- tests/puppeteer.js
+++ tests/puppeteer.js
@@ -1,3 +1,5 @@
+/* global Event */
+
 const puppeteer = require('puppeteer'); // v13.0.0 or later
 
 (async () => {
@@ -123,15 +125,11 @@ const puppeteer = require('puppeteer'); // v13.0.0 or later
   }
 
   async function waitForConnected (element, timeout) {
-    await waitForFunction(async () => {
-      return await element.getProperty('isConnected')
-    }, timeout)
+    return waitForFunction(() => element.getProperty('isConnected'), timeout)
   }
 
   async function waitForInViewport (element, timeout) {
-    await waitForFunction(async () => {
-      return await element.isIntersectingViewport({ threshold: 0 })
-    }, timeout)
+    return waitForFunction(() => element.isIntersectingViewport({ threshold: 0 }), timeout)
   }
 
   async function waitForSelector (selector, frame, options) {
@@ -162,132 +160,23 @@ const puppeteer = require('puppeteer'); // v13.0.0 or later
     return element
   }
 
-  async function waitForElement (step, frame, timeout) {
-    const {
-      count = 1,
-      operator = '>=',
-      visible = true,
-      properties,
-      attributes
-    } = step
-    const compFn = {
-      '==': (a, b) => a === b,
-      '>=': (a, b) => a >= b,
-      '<=': (a, b) => a <= b
-    }[operator]
-    await waitForFunction(async () => {
-      const elements = await querySelectorsAll(step.selectors, frame)
-      let result = compFn(elements.length, count)
-      const elementsHandle = await frame.evaluateHandle((...elements) => {
-        return elements
-      }, ...elements)
-      await Promise.all(elements.map((element) => element.dispose()))
-      if (result && (properties || attributes)) {
-        result = await elementsHandle.evaluate(
-          (elements, properties, attributes) => {
-            for (const element of elements) {
-              if (attributes) {
-                for (const [name, value] of Object.entries(attributes)) {
-                  if (element.getAttribute(name) !== value) {
-                    return false
-                  }
-                }
-              }
-              if (properties) {
-                if (!isDeepMatch(properties, element)) {
-                  return false
-                }
-              }
-            }
-            return true
-
-            function isDeepMatch (a, b) {
-              if (a === b) {
-                return true
-              }
-              if ((a && !b) || (!a && b)) {
-                return false
-              }
-              if (!(a instanceof Object) || !(b instanceof Object)) {
-                return false
-              }
-              for (const [key, value] of Object.entries(a)) {
-                if (!isDeepMatch(value, b[key])) {
-                  return false
-                }
-              }
-              return true
-            }
-          },
-          properties,
-          attributes
-        )
-      }
-      await elementsHandle.dispose()
-      return result === visible
-    }, timeout)
-  }
-
-  async function querySelectorsAll (selectors, frame) {
-    for (const selector of selectors) {
-      const result = await querySelectorAll(selector, frame)
-      if (result.length) {
-        return result
-      }
-    }
-    return []
-  }
-
-  async function querySelectorAll (selector, frame) {
-    if (!Array.isArray(selector)) {
-      selector = [selector]
-    }
-    if (!selector.length) {
-      throw new Error('Empty selector provided to querySelectorAll')
-    }
-    let elements = []
-    for (let i = 0; i < selector.length; i++) {
-      const part = selector[i]
-      if (i === 0) {
-        elements = await frame.$$(part)
-      } else {
-        const tmpElements = elements
-        elements = []
-        for (const el of tmpElements) {
-          elements.push(...(await el.$$(part)))
-        }
-      }
-      if (elements.length === 0) {
-        return []
-      }
-      if (i < selector.length - 1) {
-        const tmpElements = []
-        for (const el of elements) {
-          const newEl = (await el.evaluateHandle(el => el.shadowRoot ? el.shadowRoot : el)).asElement()
-          if (newEl) {
-            tmpElements.push(newEl)
-          }
-        }
-        elements = tmpElements
-      }
-    }
-    return elements
-  }
-
   async function waitForFunction (fn, timeout) {
-    let isActive = true
-    const timeoutId = setTimeout(() => {
-      isActive = false
-    }, timeout)
-    while (isActive) {
-      const result = await fn()
-      if (result) {
-        clearTimeout(timeoutId)
-        return
+    let isDone = false
+    let result = false
+    const check = async () => {
+      result = await fn()
+      if (!result && !isDone) {
+        setTimeout(check, 100)
       }
-      await new Promise(resolve => setTimeout(resolve, 100))
     }
-    throw new Error('Timed out')
+    const timeoutId = setTimeout(() => {
+      isDone = true
+    }, timeout)
+    await check()
+    clearTimeout(timeoutId)
+    if (!result) {
+      throw new Error('Timed out')
+    }
   }
 
   async function changeSelectElement (element, value) {
